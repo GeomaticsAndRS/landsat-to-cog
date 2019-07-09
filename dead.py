72e31bec9a1c7bc1f8fcbeac6ae65f75032fde81
EC2 Default User <ec2-user@ip-172-31-21-204.us-west-2.compute.internal>
2019-07-09 02:33:55 +0000

    junk script to test queues

--- dead.py
+++ dead.py
@@ -15,4 +15,35 @@ import copy
 import boto3
 import botocore
 
-a=3
+QUEUE = os.environ.get('QUEUE', 'dsg-test-queue')
+DLQUEUE = os.environ.get('QUEUE', 'l2c-dead-letter')
+
+
+# Set up some AWS stuff
+s3 = boto3.client('s3')
+
+s3r = boto3.resource('s3')
+sqs = boto3.resource('sqs')
+queue = sqs.get_queue_by_name(QueueName=QUEUE)
+dlqueue = sqs.get_queue_by_name(QueueName=DLQUEUE)
+
+messages = queue.receive_messages(
+        VisibilityTimeout=10,
+        MaxNumberOfMessages=1
+    )
+message = messages[0]
+print ('standard queue')
+print (message)
+
+dlqueue.send_message(MessageBody=message.body)
+
+messages = dlqueue.receive_messages(
+        VisibilityTimeout=10,
+        MaxNumberOfMessages=1
+    )
+print ('dead queue')
+message = messages[0]
+
+print (message)
+
+print('ran ok')
